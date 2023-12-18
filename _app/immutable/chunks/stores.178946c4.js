fccba74eff267c1fb39cb93a2f770b86b6b8c259
Celticoding <74544183+Celticoding@users.noreply.github.com>
2023-12-18 18:30:12 +0000

    Deploying to gh-pages from @ Celticoding/portfolio@c72233c1479bfa6466730cc70fad7098f6e2ff0e 🚀

---
+++ _app/immutable/chunks/stores.178946c4.js
@@ -0,0 +1,1 @@
+import{d as e}from"./singletons.4c6b6376.js";const r=()=>{const s=e;return{page:{subscribe:s.page.subscribe},navigating:{subscribe:s.navigating.subscribe},updated:s.updated}},b={subscribe(s){return r().page.subscribe(s)}};export{b as p};
